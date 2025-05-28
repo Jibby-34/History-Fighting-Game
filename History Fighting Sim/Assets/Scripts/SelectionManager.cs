e57eb8592cd1f534ff1a8978295721049ed0afd6
Gibson DeGroot <20120725@zpsstudents.org>
2025-05-28 16:14:06 -0400

    Working ready up system

--- History Fighting Sim/Assets/Scripts/SelectionManager.cs
+++ History Fighting Sim/Assets/Scripts/SelectionManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SelectionManager : MonoBehaviour
 {
@@ -9,20 +11,42 @@ public class SelectionManager : MonoBehaviour
     public int selector2Index = 0;  // current selected index by selector 2
     int gridWidth = 3;
     int gridHeight = 3;
+    public TextMeshProUGUI player1Ready;
+    public TextMeshProUGUI player2Ready;
+    bool player1Selected = false;
+    bool player2Selected = false;
+
+    void Start()
+    {
+        MoveSelector1(0);
+        MoveSelector2(1);
+    }
 
     void Update()
     {
-        // Move selector one (blue)
-        if (Input.GetKeyDown(KeyCode.W)) MoveSelector1(-gridWidth); // move up one row
-        if (Input.GetKeyDown(KeyCode.S)) MoveSelector1(gridWidth);  // move down one row
-        if (Input.GetKeyDown(KeyCode.A)) MoveSelector1(-1);         // left
-        if (Input.GetKeyDown(KeyCode.D)) MoveSelector1(1);          // right
+        if (!player1Selected)
+        {
+            // Move selector one (blue)
+            if (Input.GetKeyDown(KeyCode.W)) MoveSelector1(-gridWidth); // move up one row
+            if (Input.GetKeyDown(KeyCode.S)) MoveSelector1(gridWidth);  // move down one row
+            if (Input.GetKeyDown(KeyCode.A)) MoveSelector1(-1);         // left
+            if (Input.GetKeyDown(KeyCode.D)) MoveSelector1(1);          // right
+        }
 
-        // Move selector two (red)
-        if (Input.GetKeyDown(KeyCode.I)) MoveSelector2(-gridWidth);
-        if (Input.GetKeyDown(KeyCode.K)) MoveSelector2(gridWidth);
-        if (Input.GetKeyDown(KeyCode.J)) MoveSelector2(-1);
-        if (Input.GetKeyDown(KeyCode.L)) MoveSelector2(1);
+        if (!player2Selected)
+        {
+            // Move selector two (red)
+            if (Input.GetKeyDown(KeyCode.I)) MoveSelector2(-gridWidth);
+            if (Input.GetKeyDown(KeyCode.K)) MoveSelector2(gridWidth);
+            if (Input.GetKeyDown(KeyCode.J)) MoveSelector2(-1);
+            if (Input.GetKeyDown(KeyCode.L)) MoveSelector2(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.U)) SelectCharacter(selector2Index, 2);
+        if (Input.GetKeyDown(KeyCode.E)) SelectCharacter(selector1Index, 1);
+
+        if (Input.GetKeyDown(KeyCode.H)) UnselectCharacter(2);
+        if (Input.GetKeyDown(KeyCode.F)) UnselectCharacter(1);
     }
 
     void MoveSelector1(int offset)
@@ -60,6 +84,38 @@ public class SelectionManager : MonoBehaviour
 
             gridCells[selector2Index].isSelector2Here = true;
             gridCells[selector2Index].UpdateSprite();
+        }
+    }
+
+    public void SelectCharacter(int index, int player)
+    {
+        if (player == 1)
+        {
+            player1Ready.text = "Ready";
+            player1Ready.color = new Color(0.217556f, 0.9372549f, 0.1607843f); 
+            player1Selected = true;
+        }
+        else
+        {
+            player2Ready.text = "Ready";
+            player2Ready.color = new Color(0.217556f, 0.9372549f, 0.1607843f); 
+            player2Selected = true;
+        }
+    }
+    
+    public void UnselectCharacter(int player)
+    {
+        if (player == 1)
+        {
+            player1Ready.text = "Not Ready";
+            player1Ready.color = new Color(0.9371068f, 0.1620781f, 0.1620781f); 
+            player1Selected = false;
+        }
+        else
+        {
+            player2Ready.text = "Not Ready";
+            player2Ready.color = new Color(0.9371068f, 0.1620781f, 0.1620781f);
+            player2Selected = false;
         }
     }
 }
